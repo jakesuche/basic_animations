c37514183dac9a8e3abb0a370c05f343a93fafa3
Uchechukwu Chidiebere <uchechukwuchidiebere@Uchechukwus-MacBook-Pro-2.local>
2022-04-29 15:54:46 +0100

    sections added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import Hero from './components/hero'
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./components/hero";
+import * as Styles from "./home_styles.js";
 
 function App() {
   return (
     <div>
       <Hero />
+      <Styles.Section>
+        <div className="section_content">
+          <h3>Button animations</h3>
+
+          <p>
+            One question that comes up a lot is "When using hooks how do I get
+            the previous value of props or state?". With React class components
+            you have the componentDidUpdate method which receives previous props
+            and state as arguments or you can update an instance variable
+            (this.previous = value) and reference it later to get the previous
+            value. So how can we do this inside a functional component that
+            doesn't have lifecycle methods or an instance to store values on?
+            Hooks to the rescue! We can create a custom hook that uses the
+            useRef hook internally for storing the previous value. See the
+            recipe below with inline comments. You can also find this example in
+            the official React Hooks FAQ.
+          </p>
+        </div>
+        <div className="section_content">
+          <h3>Button animations</h3>
+
+          <p>
+            One question that comes up a lot is "When using hooks how do I get
+            the previous value of props or state?". With React class components
+            you have the componentDidUpdate method which receives previous props
+            and state as arguments or you can update an instance variable
+            (this.previous = value) and reference it later to get the previous
+            value. So how can we do this inside a functional component that
+            doesn't have lifecycle methods or an instance to store values on?
+            Hooks to the rescue! We can create a custom hook that uses the
+            useRef hook internally for storing the previous value. See the
+            recipe below with inline comments. You can also find this example in
+            the official React Hooks FAQ.
+          </p>
+        </div>
+      </Styles.Section>
     </div>
   );
 }
---
+++ src/home_styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+
+
+export const Section = styled.section`
+
+.section_content{
+    width:100%;
+    max-width:1300px;
+    margin:auto;
+    padding: 0px 2rem 0 2rem;
+    margin-bottom:3rem;
+}
+
+`
