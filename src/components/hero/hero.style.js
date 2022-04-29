7e653c232770ea8a256a35cf4a4639b46771c4ae
Uchechukwu Chidiebere <uchechukwuchidiebere@Uchechukwus-MacBook-Pro-2.local>
2022-04-29 15:31:34 +0100

    hero page progress

---
+++ src/components/hero/hero.style.js
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  background-image: linear-gradient(
+    141deg,
+    #009e6c 0,
+    #00d1b2 71%,
+    #00e7eb 100%
+  );
+  background-color: #00d1b2;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  height: 250px;
+  align-items: center;
+  text-align: center;
+  margin:auto .content {
+  }
+`;
+
+export const Section = styled.section`
+  background-color: rgb(233, 255, 252);
+
+  .section-inner {
+    max-width: 1300px;
+    margin: auto;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    padding: 3rem 1.5rem;
+  }
+`;
+export const Card = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  ${'' /* border: 1px solid rgba(0, 0, 0, 0.125); */}
+  border-radius: 0.25rem;
+  max-width: 500px;
+  padding: 2rem 1rem 2rem 1rem;
+  box-shadow: 0 2px 3px rgb(10 10 10 / 10%), 0 0 0 1px rgb(10 10 10 / 10%);
+  .actions {
+    display: flex;
+    justify-content: center;
+    input {
+      display: block;
+      width: 100%;
+      padding: 0.375rem 0.75rem;
+      font-size: 1rem;
+      font-weight: 500;
+      line-height: 1.5;
+      color: #212529;
+      background-color: #fff;
+      background-clip: padding-box;
+      border: 1px solid #ced4da;
+      appearance: none;
+      border-radius: 0.25rem 0rem 0rem 0.25rem;
+      transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+      max-width: 300px;
+    }
+    a{
+        border:none;
+        border-radius: 0rem 0.25rem 0.25rem 0rem;
+        background-color:#ED203D;
+        outline:none;
+       
+
+    }
+  }
+  .body {
+    flex: 1 1 auto;
+    padding: 1rem 1rem;
+    .title {
+      margin-bottom: 0.5rem;
+    }
+    .text{
+        width: 300px;
+        
+    }
+
+  }
+`;
---
+++ src/components/hero/index.js
@@ -0,0 +1,44 @@
+import * as Styles from "./hero.style";
+
+export default function () {
+  return (
+    <div>
+      <Styles.Header>
+        <div className="content">
+          <h2>BASIC ANIMATIONS</h2>
+          <p>Easy to understand basic css animations</p>
+        </div>
+      </Styles.Header>
+      <Styles.Section>
+        <div className="section-inner row">
+          <div className="col-lg-6">
+            <h4>What's all this about?</h4>
+            <p>
+              Hooks are a feature in React that allow you use state and other
+              React features without writing classes. This website provides easy
+              to understand code examples to help you learn how hooks work and
+              inspire you to take advantage of them in your next project.
+            </p>
+          </div>
+          <div className="col-lg-6">
+            <Styles.Card>
+              <div className="body">
+                <h5 className="title">Card title</h5>
+                <p className="text">
+                     Your weekly dose of CSS news. Delivered every monday to <strong>0</strong> devs, for free.
+                </p>
+                <div className="actions">
+                <input  type="" placeholder="Your Email" />
+                <a href="#" className="btn btn-primary">
+                  Shoot
+                </a>
+                </div>
+              </div>
+            </Styles.Card>
+           
+          </div>
+        </div>
+      </Styles.Section>
+    </div>
+  );
+}
