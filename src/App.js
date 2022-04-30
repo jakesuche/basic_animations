f36df33dfa27b8392aeb458a5d23ef8a162e5503
Uchechukwu Chidiebere <uchechukwuchidiebere@Uchechukwus-MacBook-Pro-2.local>
2022-04-30 05:36:31 +0100

    more animations added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import Hero from "./components/hero";
 import * as Styles from "./home_styles.js";
 import { animations } from "./utils/constants";
-import React from "react"
-import './assets/css/onscroll.css'
+import React from "react";
+import "./assets/css/onscroll.css";
 function App() {
-  
-
   const isInViewPort = (el) => {
     var rect = el.getBoundingClientRect();
     return (
-      (rect.top <= 0
-         && rect.bottom >= 0) 
-         || 
-         (rect.bottom >=  (window.innerHeight || document.documentElement.clientHeight)
-          && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) 
-          || (rect.top >= 0 &&  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) ))
-  }
+      (rect.top <= 0 && rect.bottom >= 0) ||
+      (rect.bottom >=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <=
+          (window.innerHeight || document.documentElement.clientHeight)) ||
+      (rect.top >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight))
+    );
+  };
 
-  React.useEffect(()=>{
-      var scroll = window.requestAnimationFrame || function(callback) {
-        window.setTimeout(callback, 100/60)
-      }
-      var elementsToshow = document.querySelectorAll('.show-on-scroll')
-      function loop(){
-        elementsToshow.forEach(function(element){
-          if(isInViewPort(element)){
-            element.classList.add('is-visible')
-          }else{
-            element.classList.remove('is-visible')
-          }
-        })
-        scroll(loop) 
-      }
-      loop()
-      
-  },[])
+  React.useEffect(() => {
+    var scroll =
+      window.requestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 100 / 60);
+      };
+    var elementsToshow = document.querySelectorAll(".show-on-scroll");
+    function loop() {
+      elementsToshow.forEach(function (element) {
+        if (isInViewPort(element)) {
+          element.classList.add("is-visible");
+        } else {
+          element.classList.remove("is-visible");
+        }
+      });
+      scroll(loop);
+    }
+    loop();
+  }, []);
   return (
     <div>
       <Hero />
@@ -45,11 +47,13 @@ function App() {
               <h3>{animation.title}</h3>
 
               <p>{animation.content}</p>
-              <div className="show-on-scroll" >
+              <div className="show-on-scroll">
                 <iframe
-                  style={{ width: "100%", height: "500px",margin: "auto" }}
+                  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                  style={{ width: "100%", height: "500px", margin: "auto" }}
                   src={animation.link}
                   title={animation.title}
+                  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
                 />
               </div>
             </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,6 +13,32 @@ export const animations = [
         title: "Card item hover",
         content: "Social media card hover effect",
         link: 'https://codesandbox.io/embed/3r5xid'
+    },
+    {
+        title:"Call indicator",
+        content:"call indicator animation",
+        link:"https://codesandbox.io/embed/call-indicator-animation-87tmlw?fontsize=14&hidenavigation=1&theme=dark"
+    },
+    {
+        title:"Preloader",
+        content:"Preloader animation",
+        link:"https://codesandbox.io/embed/preloader-rotate-around-8bluef?codemirror=1&fontsize=14&hidenavigation=1&theme=dark"
+    },
+    {
+        title:"Card item hover",
+        content:"Card item hover with background translate to bottom to top",
+        link:"https://codesandbox.io/embed/card-hover-effect-u8n2ph?autoresize=1&fontsize=13&hidenavigation=1&theme=dark&view=preview"
+    },
+    {
+        title:"Card with button hover",
+        content:"Cards with buttons ",
+        link:"https://codesandbox.io/embed/sharp-solomon-4352vj?fontsize=14&hidenavigation=1&theme=dark&view=preview"
+    },
+    {
+        title:"Button hover effect",
+        content:"scale on hover effect with border translate on hover list",
+        link:"https://codesandbox.io/embed/button-hover-effect-6g3psw?fontsize=14&hidenavigation=1&theme=dark&view=preview"
     }
+
 ]
 
