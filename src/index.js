4cce684980640266a3deff6d4f6c1dc3ef0ed80a
jakesuche <uchechidi9@gmail.com>
2022-04-30 19:47:06 +0100

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './assets/css/bootstrap.min.css'
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
